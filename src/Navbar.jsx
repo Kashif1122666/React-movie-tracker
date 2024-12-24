306ec0583ecbfb232b7042d08aa02797bba3d3d3
kashif1122666 <kashif1122666@gmail.com>
2024-12-24 10:35:52 +0500

    add isactive status to the navigation buttons

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link} from 'react-router-dom'
+import { Link,NavLink} from 'react-router-dom'
 
 function Navbar() {
   return (
   <>
   <ul className='list-none flex justify-center gap-20  p-5 py-6 bg-[#2E4053] text-white fixed top-0 w-[1200px] rounded-lg '>
-    <li><Link to="/" className='border border-black px-5 py-3 rounded-lg bg-orange-600 hover:bg-orange-700  hover:px-6'>Home</Link></li>
-    <li><Link to="/favoriteList" className='border border-black px-3 py-3 rounded-lg bg-orange-600 hover:bg-orange-700  hover:px-6'>Favorite</Link></li>
+    <li><NavLink to="/" className={({isActive})=> `border border-black px-5 py-3 rounded-lg ${isActive ? "bg-blue-900": "bg-orange-600"} hover:bg-blue-900  hover:px-6`}>Home</NavLink></li>
+    <li><NavLink to="/favoriteList" className={({isActive})=>`border border-black px-3 py-3 rounded-lg  ${isActive ? "bg-blue-900":"bg-orange-600"} hover:bg-blue-900  hover:px-6`} >Favorite</NavLink></li>
   </ul>
   </>
   )
